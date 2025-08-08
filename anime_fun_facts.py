fda83dcb7882303cf6a6a650c61adb876e09705d
gitmobkab <richmondaurel77@gmail.com>
2025-08-08 20:23:43 +0000

    updated the get_random_anime function

--- anime_fun_facts.py
+++ anime_fun_facts.py
@@ -3,25 +3,26 @@ import rich
 from ascii_magic import AsciiArt
 import random
 
-def get_random_anime(numbers_of_requests: int = 1):
+def get_random_anime():
     request = requests.get("https://api.jikan.moe/v4/random/anime")
     request_json = request.json()
     data = request_json["data"]
     if request.status_code == 200:
         images = data["images"]
         webps = images["webp"]
-        draw_ascii_image(webps["large_image_url"])
-        return data["title"],data["score"],data["score_by"],data["rank"],data["popularity"]
+        return data["title"],data["score"],data["scored_by"],data["rank"],data["popularity"],webps["large_image_url"]
     else:
-        return 0
+        return -1
     
     
     
 def draw_ascii_image(url:str):
     try:
         ascii_art = AsciiArt.from_url(url)
-        ascii_art.to_terminal()
+        return ascii_art.to_ascii()
     except Exception as error:
         print(f"Couldn't load image, error name: {error.__str__}")
     
-get_random_anime()
+
+if __name__ == "__main__":
+    get_random_anime()
